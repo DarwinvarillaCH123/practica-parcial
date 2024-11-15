4aa86e968421bf98a7ce6583b54264b18ad56297
Darwin <darwin.varillach@amigo.edu.co>
2024-11-15 17:49:10 -0500

    modifique el menu de el index

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 document.getElementById('linkDiv').onclick = function() {
-    window.open('https://youtu.be/CKI8iQTgZKU','_blank')
+    window.open('https://es.aliexpress.com/item/1005007208813103.html?aff_platform=true&aff_short_key=UneMJZVf&isdl=y&src=bing&albch=shopping&acnt=135095331&isdl=y&albcp=555157127&albag=1310619016253918&slnk=&trgt=pla-4585513252419848&plac=&crea=81913744742423&netw=o&device=c&mtctp=e&utm_source=Bing&utm_medium=shopping&utm_campaign=PA_Bing_CO_PLA_PC%2FM_ALL_20241030_Others&utm_content=ALL&utm_term=jamorant%20zapatos&msclkid=bdaa354808ce1b353d44527fc7b00843','_blank')
 }
 
 document.getElementById('linkDiv2').onclick = function() {
