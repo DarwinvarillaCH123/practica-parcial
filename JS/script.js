a38819d46f231c019eeec83e84f23513b82c3773
Darwin <darwin.varillach@amigo.edu.co>
2024-11-15 13:08:10 -0500

    pagina sencilla de html, css y Js

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+document.getElementById('linkDiv').onclick = function() {
+    window.open('https://youtu.be/CKI8iQTgZKU','_blank')
+}
+
+document.getElementById('linkDiv2').onclick = function() {
+    window.open('https://www.youtube.com/watch?v=TGgcC5xg9YI&pp=ygUfc2VlIHlvdSBhZ2FpbiB0eWxlciB0aGUgY3JlYXRvcg%3D%3D','_blank')
+}
+
+
+document.getElementById('linkDiv3').onclick = function () {
+    window.open('https://youtu.be/wfplQ20bfjA?si=KnzB2SPdK6G_vCOR','_blank')
+}
